eda215cf67f54dc7aba84f3c1d9916366e20c446
SevvalKolaca <sevval.kolaca.00@gmail.com>
2025-06-18 16:11:06 +0300

    JPQL added for get genre name.
    better for memory management.

--- src/main/java/com/moviender/moviender/movieAndGenre/repository/GenreRepository.java
+++ src/main/java/com/moviender/moviender/movieAndGenre/repository/GenreRepository.java
@@ -2,9 +2,14 @@ package com.moviender.moviender.movieAndGenre.repository;
 
 import com.moviender.moviender.movieAndGenre.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
     List<Genre> findByIdIn(List<Integer> genreId);
+
+    @Query("SELECT g.genreName FROM Genre g WHERE g.id IN :ids")
+    List<String> findGenreNamesByIds(@Param("ids") List<Integer> ids);
 }
